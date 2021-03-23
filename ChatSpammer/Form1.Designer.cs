16afff202607a91d2311fcdb9f4130296f57290a
Nazar121110 <80525812+Nazar121110@users.noreply.github.com>
2021-03-23 10:52:10 +0000

    jdchat
    qqqqewqwe

---
+++ ChatSpammer/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace ChatSpammer
+{
+    partial class MainHub
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainHub));
+            this.MainHubElipse = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.MainHubDragControl = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.bunifuImageButton1 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.bunifuImageButton2 = new Bunifu.Framework.UI.BunifuImageButton();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.LoginGradientButton = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.guna2GradientButton1 = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SpamBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.guna2GradientButton2 = new Guna.UI2.WinForms.Guna2GradientButton();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainHubElipse
+            // 
+            this.MainHubElipse.BorderRadius = 35;
+            this.MainHubElipse.TargetControl = this;
+            // 
+            // MainHubDragControl
+            // 
+            this.MainHubDragControl.TargetControl = this;
+            // 
+            // bunifuImageButton1
+            // 
+            this.bunifuImageButton1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton1.Image")));
+            this.bunifuImageButton1.ImageActive = null;
+            this.bunifuImageButton1.Location = new System.Drawing.Point(435, 8);
+            this.bunifuImageButton1.Name = "bunifuImageButton1";
+            this.bunifuImageButton1.Size = new System.Drawing.Size(36, 24);
+            this.bunifuImageButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton1.TabIndex = 0;
+            this.bunifuImageButton1.TabStop = false;
+            this.bunifuImageButton1.Zoom = 10;
+            this.bunifuImageButton1.Click += new System.EventHandler(this.bunifuImageButton1_Click);
+            // 
+            // bunifuImageButton2
+            // 
+            this.bunifuImageButton2.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuImageButton2.Image = ((System.Drawing.Image)(resources.GetObject("bunifuImageButton2.Image")));
+            this.bunifuImageButton2.ImageActive = null;
+            this.bunifuImageButton2.Location = new System.Drawing.Point(386, 8);
+            this.bunifuImageButton2.Name = "bunifuImageButton2";
+            this.bunifuImageButton2.Size = new System.Drawing.Size(36, 27);
+            this.bunifuImageButton2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bunifuImageButton2.TabIndex = 1;
+            this.bunifuImageButton2.TabStop = false;
+            this.bunifuImageButton2.Zoom = 10;
+            this.bunifuImageButton2.Click += new System.EventHandler(this.bunifuImageButton2_Click);
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox1.Image")));
+            this.guna2PictureBox1.Location = new System.Drawing.Point(161, 8);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.ShadowDecoration.Parent = this.guna2PictureBox1;
+            this.guna2PictureBox1.Size = new System.Drawing.Size(149, 102);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2PictureBox1.TabIndex = 2;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // LoginGradientButton
+            // 
+            this.LoginGradientButton.Animated = true;
+            this.LoginGradientButton.AutoRoundedCorners = true;
+            this.LoginGradientButton.BackColor = System.Drawing.Color.Transparent;
+            this.LoginGradientButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.LoginGradientButton.BorderRadius = 17;
+            this.LoginGradientButton.CheckedState.Parent = this.LoginGradientButton;
+            this.LoginGradientButton.CustomImages.Parent = this.LoginGradientButton;
+            this.LoginGradientButton.FillColor = System.Drawing.Color.Crimson;
+            this.LoginGradientButton.FillColor2 = System.Drawing.Color.DarkRed;
+            this.LoginGradientButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.LoginGradientButton.ForeColor = System.Drawing.Color.Transparent;
+            this.LoginGradientButton.HoverState.Parent = this.LoginGradientButton;
+            this.LoginGradientButton.Image = ((System.Drawing.Image)(resources.GetObject("LoginGradientButton.Image")));
+            this.LoginGradientButton.ImageSize = new System.Drawing.Size(30, 30);
+            this.LoginGradientButton.Location = new System.Drawing.Point(142, 285);
+            this.LoginGradientButton.Name = "LoginGradientButton";
+            this.LoginGradientButton.ShadowDecoration.Parent = this.LoginGradientButton;
+            this.LoginGradientButton.Size = new System.Drawing.Size(198, 36);
+            this.LoginGradientButton.TabIndex = 9;
+            this.LoginGradientButton.Click += new System.EventHandler(this.LoginGradientButton_Click);
+            // 
+            // guna2GradientButton1
+            // 
+            this.guna2GradientButton1.Animated = true;
+            this.guna2GradientButton1.AutoRoundedCorners = true;
+            this.guna2GradientButton1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2GradientButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.guna2GradientButton1.BorderRadius = 17;
+            this.guna2GradientButton1.CheckedState.Parent = this.guna2GradientButton1;
+            this.guna2GradientButton1.CustomImages.Parent = this.guna2GradientButton1;
+            this.guna2GradientButton1.FillColor = System.Drawing.Color.Crimson;
+            this.guna2GradientButton1.FillColor2 = System.Drawing.Color.DarkRed;
+            this.guna2GradientButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2GradientButton1.ForeColor = System.Drawing.Color.Transparent;
+            this.guna2GradientButton1.HoverState.Parent = this.guna2GradientButton1;
+            this.guna2GradientButton1.Image = ((System.Drawing.Image)(resources.GetObject("guna2GradientButton1.Image")));
+            this.guna2GradientButton1.ImageSize = new System.Drawing.Size(30, 30);
+            this.guna2GradientButton1.Location = new System.Drawing.Point(142, 225);
+            this.guna2GradientButton1.Name = "guna2GradientButton1";
+            this.guna2GradientButton1.ShadowDecoration.Parent = this.guna2GradientButton1;
+            this.guna2GradientButton1.Size = new System.Drawing.Size(198, 36);
+            this.guna2GradientButton1.TabIndex = 10;
+            this.guna2GradientButton1.Click += new System.EventHandler(this.guna2GradientButton1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 372);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "©SkyTools";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SpamBox1
+            // 
+            this.SpamBox1.AutoRoundedCorners = true;
+            this.SpamBox1.BorderColor = System.Drawing.Color.Gainsboro;
+            this.SpamBox1.BorderRadius = 17;
+            this.SpamBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.SpamBox1.DefaultText = "";
+            this.SpamBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.SpamBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.SpamBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.SpamBox1.DisabledState.Parent = this.SpamBox1;
+            this.SpamBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.SpamBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.SpamBox1.FocusedState.Parent = this.SpamBox1;
+            this.SpamBox1.Font = new System.Drawing.Font("Calibri", 11.25F);
+            this.SpamBox1.ForeColor = System.Drawing.Color.DarkGray;
+            this.SpamBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.SpamBox1.HoverState.Parent = this.SpamBox1;
+            this.SpamBox1.Location = new System.Drawing.Point(142, 147);
+            this.SpamBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.SpamBox1.Name = "SpamBox1";
+            this.SpamBox1.PasswordChar = '\0';
+            this.SpamBox1.PlaceholderText = "Enter Spam Text";
+            this.SpamBox1.SelectedText = "";
+            this.SpamBox1.ShadowDecoration.Parent = this.SpamBox1;
+            this.SpamBox1.Size = new System.Drawing.Size(198, 36);
+            this.SpamBox1.TabIndex = 12;
+            this.SpamBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // guna2GradientButton2
+            // 
+            this.guna2GradientButton2.Animated = true;
+            this.guna2GradientButton2.AutoRoundedCorners = true;
+            this.guna2GradientButton2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2GradientButton2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.guna2GradientButton2.BorderRadius = 17;
+            this.guna2GradientButton2.CheckedState.Parent = this.guna2GradientButton2;
+            this.guna2GradientButton2.CustomImages.Parent = this.guna2GradientButton2;
+            this.guna2GradientButton2.FillColor = System.Drawing.Color.Crimson;
+            this.guna2GradientButton2.FillColor2 = System.Drawing.Color.DarkRed;
+            this.guna2GradientButton2.Font = new System.Drawing.Font("Yu Gothic UI", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2GradientButton2.ForeColor = System.Drawing.Color.White;
+            this.guna2GradientButton2.HoverState.Parent = this.guna2GradientButton2;
+            this.guna2GradientButton2.ImageSize = new System.Drawing.Size(30, 30);
+            this.guna2GradientButton2.Location = new System.Drawing.Point(142, 341);
+            this.guna2GradientButton2.Name = "guna2GradientButton2";
+            this.guna2GradientButton2.ShadowDecoration.Parent = this.guna2GradientButton2;
+            this.guna2GradientButton2.Size = new System.Drawing.Size(198, 36);
+            this.guna2GradientButton2.TabIndex = 13;
+            this.guna2GradientButton2.Text = "Send DM For Help";
+            this.guna2GradientButton2.Click += new System.EventHandler(this.guna2GradientButton2_Click);
+            // 
+            // MainHub
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(483, 389);
+            this.Controls.Add(this.guna2GradientButton2);
+            this.Controls.Add(this.SpamBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2GradientButton1);
+            this.Controls.Add(this.LoginGradientButton);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.Controls.Add(this.bunifuImageButton2);
+            this.Controls.Add(this.bunifuImageButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainHub";
+            this.Text = "Spam Home ";
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bunifuImageButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Elipse MainHubElipse;
+        private Guna.UI2.WinForms.Guna2DragControl MainHubDragControl;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton2;
+        private Bunifu.Framework.UI.BunifuImageButton bunifuImageButton1;
+        private Guna.UI2.WinForms.Guna2GradientButton guna2GradientButton1;
+        private Guna.UI2.WinForms.Guna2GradientButton LoginGradientButton;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2TextBox SpamBox1;
+        private System.Windows.Forms.Timer timer1;
+        private Guna.UI2.WinForms.Guna2GradientButton guna2GradientButton2;
+    }
+}
+
---
+++ ChatSpammer/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatSpammer
+{
+    public partial class MainHub : Form
+    {
+        public MainHub()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void bunifuImageButton2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void guna2GradientButton1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void LoginGradientButton_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendKeys.Send(SpamBox1.Text);
+            SendKeys.Send("{Enter}");
+
+        }
+
+        private void guna2GradientButton2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.instagram.com/Preciselydev/");
+        }
+    }
+}
